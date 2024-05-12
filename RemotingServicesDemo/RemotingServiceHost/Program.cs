0e41475d4aa2d0627703a0a1e9ff4dc329c2a0d9
Mysttic <19372942+Mysttic@users.noreply.github.com>
2024-05-12 11:50:01 +0200

    Part 2 of tutorial

---
+++ RemotingServicesDemo/RemotingServiceHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace RemotingServiceHost
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HelloRemotingService.HelloRemotingService remotingService = new HelloRemotingService.HelloRemotingService();
+            TcpChannel tcpChannel = new TcpChannel(8085);
+            ChannelServices.RegisterChannel(tcpChannel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(HelloRemotingService.HelloRemotingService), "GetMessage", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebServicesDemo/HelloWebServer/HelloWebService.asmx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesDemo
+{
+    /// <summary>
+    /// Summary description for HelloWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class HelloWebService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string GetMessage(string name)
+        {
+            return $"Hello {name}";
+        }
+    }
+}
