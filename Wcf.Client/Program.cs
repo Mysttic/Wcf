9b373eadd758e980eeb4cb01a581f2244bd45086
Mysttic <19372942+Mysttic@users.noreply.github.com>
2024-05-12 11:04:49 +0200

    Add project files.

---
+++ Wcf.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Wcf.Client
+{
+    internal class Program
+    {
+        [ServiceContract]
+        public interface IMessageService
+        {
+            [OperationContract]
+            string[] GetMessages(string nickname);
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to call the service");
+            Console.ReadLine();
+            var address = "net.tcp://localhost:8080/MessageService";
+            var binding = new NetTcpBinding(SecurityMode.None);
+            var factory = new ChannelFactory<IMessageService>(binding, address);
+            var channel = factory.CreateChannel();
+            var messages = channel.GetMessages("Mysttic");
+            foreach (var m in messages)
+            {
+                Console.WriteLine(m);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Wcf.Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Wcf.Server
+{
+    internal class Program
+    {
+        [ServiceContract]
+        public interface IMessageService
+        {
+            [OperationContract]
+            string[] GetMessages(string nickname);
+        }
+
+        [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+        public class MessageService : IMessageService
+        {
+            public string[] GetMessages(string nickname)
+            {
+                Console.WriteLine($"GetMessages called by {nickname}");
+                return new string[] { "Hello", "World" };
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            var uris = new Uri[1];
+            string address = "net.tcp://localhost:8080/MessageService";
+            uris[0] = new Uri(address);
+            IMessageService message = new MessageService();
+            ServiceHost host = new ServiceHost(message, new Uri(address));
+            var binding = new NetTcpBinding(SecurityMode.None);
+            host.AddServiceEndpoint(typeof(IMessageService), binding, address);
+            host.Opened += (sender, eventArgs) => Console.WriteLine("Service is running");
+            host.Open();
+            Console.ReadLine();
+        }
+    }
+}
